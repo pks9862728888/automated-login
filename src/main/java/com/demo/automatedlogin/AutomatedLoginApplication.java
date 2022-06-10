e2c8771b1858cfe473774a2f9163b8b4498c40a4
pks9862728888 <pks9862728888@gmail.com>
2022-06-10 23:43:07 +0530

    Initial commit

---
+++ src/main/java/com/demo/automatedlogin/AutomatedLoginApplication.java
@@ -0,0 +1,13 @@
+package com.demo.automatedlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedLoginApplication.class, args);
+	}
+
+}
